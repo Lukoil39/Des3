e110362c2b2468536a53c9ba160bdd2d3b3cd37f
Lukoil39 <goshavasilevskiy@mail.ru>
2020-05-22 13:47:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/des3/RaisedActivity.java
@@ -0,0 +1,13 @@
+package com.example.des3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RaisedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raised);
+    }
+}
